e4d3e95ff12f34f4140ef69c31e743c7a8ddcadb
Daniel Kennedy <danwkennedy@gmail.com>
2012-02-23 19:22:28 -0500

    Added the NLPEngine to the repository and set up the gitignore file for VS.

---
+++ NLPEngine/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace NLPEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process wordNet = new Process();
+
+            wordNet.StartInfo.FileName = "C:\\Program Files (x86)\\WordNet\\2.1\\bin\\wn";
+            wordNet.StartInfo.Arguments = "grab -synsv";
+            wordNet.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            wordNet.StartInfo.RedirectStandardOutput = true;
+            wordNet.StartInfo.UseShellExecute = false;
+            wordNet.Start();
+
+            System.IO.StreamReader myOutput = wordNet.StandardOutput;
+            wordNet.WaitForExit(3000);
+
+            if (wordNet.HasExited)
+            {
+                while (!myOutput.EndOfStream)
+                {
+                    String line = myOutput.ReadLine();
+                    if (line.Contains("Sense 1"))
+                    {
+                        //grab the next two lines, process them, then break
+                        String syns1 = myOutput.ReadLine();
+                        String syns2 = myOutput.ReadLine();
+                        List<String> syns = new List<String>();
+                        Console.WriteLine(syns1);
+                        Console.WriteLine(syns2);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
